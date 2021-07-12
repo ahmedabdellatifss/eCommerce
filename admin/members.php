0c5ab3d51ad9c424d5cd550b6ff3a37962b8f52c
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2021-07-12 13:03:21 +0200

    Create Main Table Design

--- admin/members.php
+++ admin/members.php
@@ -54,9 +54,9 @@
                                 echo "<td>" . $row['FullName'] . "</td>";
                                 echo "<td></td>";
                                 echo "<td> 
-                                      <a href='members.php?do=Edit&userid=" . $row['UserID'] ."' class='btn btn-success'>Edit</a> 
-                                      <a href='members.php?do=Delete&userid=" . $row['UserID'] ."' class='btn btn-danger confirm'>Delete</a> 
-                                     </td>";
+                                        <a href='members.php?do=Edit&userid=" . $row['UserID'] ."' class='btn btn-success'><i class='fa fa-edit'></i>Edit</a> 
+                                        <a href='members.php?do=Delete&userid=" . $row['UserID'] ."' class='btn btn-danger confirm'><i class='fa fa-close'></i>Delete</a> 
+                                    </td>";
                                 
                             echo "</tr>";
                         }
@@ -65,7 +65,7 @@
 
                </table>
                </div>
-               <a href="members.php?do=Add" class="btn btn-primary"> <i class="fa fa-plus" ></i> Add New Member</a>
+               <a href="members.php?do=Add" class="btn btn-primary"> <i class="fa fa-plus" ></i> New Member</a>
         </div>
 
   <?php }elseif($do == 'Add'){  // Add Members Page ?>
