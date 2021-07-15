0d9289e72fd944e458d88b9b177297281cf4775a
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2021-07-15 21:29:32 +0200

    Dashboard -Design Layout

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,13 +1,75 @@
 <?php 
-     session_start();
+    session_start();
+
+    if(isset($_SESSION['Username'])) {
     
-     if(isset($_SESSION['Username'])) {
-       
-      $pageTitle = 'Dashboard';
+    $pageTitle = 'Dashboard';
 
         include 'init.php';
         
-        
+        // Start Dashboard page
+        ?>
+        <div class="home-stats">
+            <div class="container text-center">
+                <h1>Dashboard Page </h1>
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="stat">
+                            Total Members
+                            <span>200</span>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="stat">
+                            pending Members
+                            <span>25</span>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="stat">
+                            Total Items
+                            <span>1500</span>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="stat">
+                            Total Comments
+                            <span>3500 </span>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+        </div>
+        <div class="latest">
+            <div class="container">
+                <div class="row">
+                    <div class="conl-sm-6">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <i class="fa fa-users">Latest Registerd Users</i>
+                            </div>
+                            <div class="panel-body">
+                                Test
+                            </div>
+                        </div>
+                    </div>
+                    <div class="conl-sm-6">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <i class="fa fa-tag">Latest Items </i>
+                            </div>
+                            <div class="panel-body">
+                                Test
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<?php
+        // End Dashboard page
 
         include  $tpl . 'footer.php';
 
