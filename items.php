4dd5d68c68f998d31cc93b30dcbc1ae0244b0811
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2021-09-15 22:50:34 +0200

    Show Item Page Part 2

--- items.php
+++ items.php
@@ -10,20 +10,57 @@
 
     // Select All Data Depend on this ID
 
-    $stmt = $con->prepare("SELECT * FROM  items  WHERE  item_ID = ? "); 
+    $stmt = $con->prepare("SELECT 
+                                items.*,
+                                categories.Name AS category_name,
+                                users.Username 
+                            FROM 
+                                items
+                            INNER JOIN 
+                                categories 
+                            ON 
+                                categories.ID = items.Cat_ID
+                            INNER JOIN
+                                users 
+                            ON 
+                                users.UserID = items.Member_ID      
+                            WHERE  
+                                item_ID = ? "); 
 
     // Execute Query
     $stmt->execute(array($itemid));
+
+    $count = $stmt->rowCount();
+
+    if ($count > 0 ) {
     // Fetch The data
     $item = $stmt->fetch();
-
 ?>
 <h1 class="text-center"><?php echo $item['Name'] ?></h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <img class="img-responsive img-thumbnail center-block" src="img.png" alt="" />
+        </div>
+        <div class="col-md-9">
+            <h2><?php echo $item['Name'] ?></h2>
+            <p><?php echo $item['Description'] ?></p>
+            <span><?php echo $item['Add_Date'] ?></span>
+            <div>Price: $<?php echo $item['Price'] ?></div>
+            <div>Made In: <?php echo $item['Country_Made'] ?></div>
+            <div>Category: <?php echo $item['category_name'] ?></div>
+            <div>Added By: <?php echo $item['Username'] ?></div>
 
-
+        </div>
+    </div>
+</div>
 
 <?php
-      
+
+    } else {
+        echo 'There\'s no such Id';
+    }
+
     include  $tpl . 'footer.php';
     ob_end_flush();
 ?>
